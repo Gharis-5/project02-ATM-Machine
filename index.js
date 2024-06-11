2e3922a3d5ab56da5ef879eabcdbee01d17aa232
Gharis Khalil <gharissheikh678@gmail.com>
2024-06-11 10:52:44 +0500

    Update

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let myBalance = 10000; // Dollars
 let myPin = 1234;
-let pinAnswer = await inquirer.prompt([{
+let pinAnswer = await inquirer.prompt([
+    {
         name: "pin",
         message: "Enter your pin",
-        type: "number"
-    }]);
+        type: "number",
+    },
+]);
 if (pinAnswer.pin === myPin) {
     console.log("Correct Pin Code");
-    let operationAns = await inquirer.prompt([{
+    let operationAns = await inquirer.prompt([
+        {
             name: "operator",
             message: "Select one of the operator",
             type: "list",
             choices: ["Withdraw", "Check Balance"],
-        }]);
+        },
+    ]);
     if (operationAns.operator === "Withdraw") {
-        let amountAns = await inquirer.prompt([{
+        let amountAns = await inquirer.prompt([
+            {
                 name: "amount",
                 message: "Enter your amount",
-                type: "number"
-            }]);
+                type: "number",
+            },
+        ]);
         if (myBalance >= amountAns.amount) {
-            console.log(`Your remaining balance is : ${myBalance -= amountAns.amount}`);
+            console.log(`Your remaining balance is : ${(myBalance -= amountAns.amount)}`);
         }
         else {
             console.log("Your maximum amount is 10000");
